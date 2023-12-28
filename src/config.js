2371724278563b3cd4ec394ac9f0c60dbab58ea4
Nick Angelou <angelou.nick@gmail.com>
2023-12-28 15:10:07 -0600

    update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 const { NODE_ENV, REACT_APP_MORFIX_FRONTEND_HOST, REACT_APP_SENTRY_DSN, REACT_APP_GTM_ID } = process.env
 const config = {
   NODE_ENV,
-  FRONTEND_HOST: REACT_APP_MORFIX_FRONTEND_HOST || 'https://s0l0ist.github.io/seal-sandbox',
+  FRONTEND_HOST: REACT_APP_MORFIX_FRONTEND_HOST || 'https://s0l0ist.github.io',
   ROUTES: {
-    HOME: ''
+    HOME: '/seal-sandbox'
   },
   SENTRY_DSN: REACT_APP_SENTRY_DSN || 'https://3a6178c24e7847bebb8a02e341cce224@o281265.ingest.sentry.io/1506382',
   GTM_ID: REACT_APP_GTM_ID || 'GTM-5DXRW5L',
